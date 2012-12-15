d8437640a38305786b0ba861399dac6e936229d4
Anders Bennehag <anders@bennehag.com>
2012-12-15 23:47:09 +0100

    Added event-handler. Sprite follows mouse cursor as example of this

---
+++ src/event_handler/event_handler.h
@@ -0,0 +1,61 @@
+
+
+#ifndef EVENT_HANDLER_H_
+#define EVENT_HANDLER_H_
+
+#include <SFML/Graphics.hpp>
+
+
+
+struct UserEvents
+{
+	bool pressingForward;
+	bool pressingBackward;
+	bool pressingLeft;
+	bool pressingRight;
+	bool pressingSpace;
+
+	bool pressingLMB;
+	bool pressingRMB;
+	unsigned int mouse_x;
+	unsigned int mouse_y;
+
+
+};
+
+
+class EventHandler
+{
+
+public:
+	EventHandler( sf::RenderWindow *App );
+	~EventHandler() {};
+
+	const UserEvents processEvents();
+
+
+
+
+private:
+
+
+	sf::RenderWindow *App;
+
+	UserEvents events;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #include "config.h"
 #include "res_handler.h"
 #include "particle.h"
+#include "event_handler.h"
+
+
+
 
 
 int main(int argc, char* argv[]) 
@@ -13,11 +17,15 @@ int main(int argc, char* argv[])
 
 	// Create the main rendering window
 	sf::RenderWindow App(sf::VideoMode(800, 600, 32), "SFML Graphics");
-
+	App.UseVerticalSync(true);
+	App.SetFramerateLimit(60);
 
 	ResourceHandler resHandler;
-
 	resHandler.loadAllResources();
+
+	EventHandler eventHandler(&App);
+
+	float x=0,y=0;
 
 	sf::Image* shipImage = resHandler.getSprite(std::string("res/sprites/ship1.png"));
 
@@ -27,14 +35,14 @@ int main(int argc, char* argv[])
 	// Start game loop
 	while (App.IsOpened())
 	{
-		// Process events
-		sf::Event Event;
-		while (App.GetEvent(Event))
-		{
-			// Close window : exit
-			if (Event.Type == sf::Event::Closed)
-				App.Close();
-		}
+		UserEvents events;
+		events = eventHandler.processEvents();
+		x += (events.mouse_x-x)*0.010;
+		y += (events.mouse_y-y)*0.010;
+		shipSprite.SetX(x);
+		shipSprite.SetY(y);
+
+
 
 		// Clear the screen (fill it with black color)
 		App.Clear();
